3ed7f3070b0d9174c6ca9f1681236f51c46061bc
Mateusz Korus <ogkorus@gmail.com>
2023-09-19 03:47:39 +0200

    manual calculations for 2nd octave and moved it like 2 octaves down

--- script.js
+++ script.js
@@ -36,6 +36,52 @@ const pitchesToModify = {
 81: [-1200 + next19TetStep * 19, 21]
 };
 
+
+pitchesToModifyNew = {}
+
+for (let i = 48; i < 82; i++) {
+	pitchesToModifyNew[i - 24] = [...pitchesToModify[i]]
+	pitchesToModifyNew[i - 24][1] -= 24
+}
+pitchesToModifyNew[24] = [0, -24]
+pitchesToModifyNew[25] = [-100, -24]
+pitchesToModifyNew[58] = [pitchesToModifyNew[57][0], pitchesToModifyNew[57][1] + 1] // double last note
+pitchesToModifyNew[59] = [-100 + next19TetStep, -2]
+pitchesToModifyNew[60] = [-100 + next19TetStep * 2, -1]
+pitchesToModifyNew[61] = [-100 + next19TetStep * 2, 0]
+pitchesToModifyNew[62] = [-100 + next19TetStep * 3, 1]
+pitchesToModifyNew[63] = [-100 + next19TetStep * 3, 2]
+pitchesToModifyNew[64] = [-200 + next19TetStep * 4, 2]
+pitchesToModifyNew[65] = [-400 + next19TetStep * 5, 1]
+pitchesToModifyNew[66] = [-400 + next19TetStep * 5, 2]
+pitchesToModifyNew[67] = [-400 + next19TetStep * 6, 3]
+pitchesToModifyNew[68] = [-400 + next19TetStep * 6, 4]
+pitchesToModifyNew[69] = [-400 + next19TetStep * 7, 5]
+pitchesToModifyNew[70] = [-400 + next19TetStep * 7, 6]
+pitchesToModifyNew[71] = [-600 + next19TetStep * 8, 5]
+pitchesToModifyNew[72] = [-600 + next19TetStep * 9, 6]
+pitchesToModifyNew[73] = [-600 + next19TetStep * 9, 7]
+pitchesToModifyNew[74] = [-700 + next19TetStep * 10, 7]
+pitchesToModifyNew[75] = [-700 + next19TetStep * 10, 8]
+pitchesToModifyNew[76] = [-700 + next19TetStep * 11, 9]
+pitchesToModifyNew[77] = [-700 + next19TetStep * 12, 10]
+pitchesToModifyNew[78] = [-700 + next19TetStep * 12, 11]
+pitchesToModifyNew[79] = [-900 + next19TetStep * 13, 10]
+pitchesToModifyNew[80] = [-900 + next19TetStep * 13, 11]
+pitchesToModifyNew[81] = [-900 + next19TetStep * 14, 12]
+pitchesToModifyNew[82] = [-900 + next19TetStep * 14, 13]
+pitchesToModifyNew[83] = [-900 + next19TetStep * 15, 14]
+pitchesToModifyNew[84] = [-1100 + next19TetStep * 16, 13]
+pitchesToModifyNew[85] = [-1100 + next19TetStep * 16, 14]
+pitchesToModifyNew[86] = [-1100 + next19TetStep * 17, 15]
+pitchesToModifyNew[87] = [-1100 + next19TetStep * 17, 16]
+pitchesToModifyNew[88] = [-1200 + next19TetStep * 18, 16]
+pitchesToModifyNew[89] = [-1200 + next19TetStep * 19, 17]
+pitchesToModifyNew[90] = [-1200 + next19TetStep * 19, 18]
+
+
+
+
 Object.keys(pitchesToModify).forEach(key => {
 	if (Math.abs(pitchesToModify[key][0]) > 100) {
 		Trace("too much detune")
@@ -44,14 +90,22 @@ Object.keys(pitchesToModify).forEach(key => {
 	}
 })
 
+Object.keys(pitchesToModifyNew).forEach(key => {
+	if (Math.abs(pitchesToModifyNew[key][0]) > 100) {
+		Trace("too much detune")
+		Trace(key)
+		Trace(pitchesToModify[key])
+	}
+})
 
 function transformEvent(event) {
-    if (pitchesToModify[event.pitch]){
-    if (pitchesToModify[event.pitch][0]) {
-    event.detune = pitchesToModify[event.pitch][0]
+    if (pitchesToModifyNew[event.pitch]){
+    Trace("modifying")
+    if (pitchesToModifyNew[event.pitch][0]) {
+    event.detune = pitchesToModifyNew[event.pitch][0]
     }
-    if (pitchesToModify[event.pitch][1]) {
-    event.pitch -= pitchesToModify[event.pitch][1]
+    if (pitchesToModifyNew[event.pitch][1]) {
+    event.pitch -= pitchesToModifyNew[event.pitch][1]
     }
     }
     return event
@@ -63,16 +117,18 @@ activeNotes = {}
 function HandleMIDI(event) {
 	
 	event.trace()
-
+/*
 	Trace(Object.keys(event));
 	Trace(event.toString())
 	Trace(event.data1)
+	*/
 	
     originalPitch = event.pitch
   
     if (event instanceof NoteOn){
     var on = new NoteOn
     on.pitch = event.pitch
+    on.velocity = event.velocity
     on = transformEvent(on)
     on.send(); //send the note
     
@@ -88,5 +144,5 @@ function HandleMIDI(event) {
     delete activeNotes[originalPitch]
     Trace("sending")
     return off.send()
-    }                      
+    }                     
 }
